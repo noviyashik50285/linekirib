922aac886523216e60b050ea08bf7582bb9408c0
noviyashik50285 <mynewdisk50285@gmail.com>
2025-05-18 00:44:50 +0300

    first commit

---
+++ perekluch.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){ 
+    $(".poyaischkn1").click(function(){
+        $(".pokaz1").show();
+        $(".pokaz2").hide();
+        $(".pokaz3").hide();
+        $(".pokaz4").hide();
+    }); 
+    $(".poyaischkn2").click(function(){
+        $(".pokaz1").hide();
+        $(".pokaz2").show();
+        $(".pokaz3").hide();
+        $(".pokaz4").hide();
+    });
+    $(".poyaischkn3").click(function(){
+        $(".pokaz1").hide();
+        $(".pokaz2").hide();
+        $(".pokaz3").show();
+        $(".pokaz4").hide();
+    });
+    $(".poyaischkn4").click(function(){
+        $(".pokaz1").hide();
+        $(".pokaz2").hide();
+        $(".pokaz3").hide();
+        $(".pokaz4").show();
+    });
+});
